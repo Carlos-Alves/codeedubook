f9e435aebd2b84f31d97b8a61caadbda350e2bbf
Carlos-Alves <carlos.alves.ti@gmail.com>
2017-01-18 01:02:28 +0000

    Modulo CodeEduBook finalizado

--- Http/routes.php
+++ Http/routes.php
@@ -1,3 +1,12 @@
 <?php
+Route::group(['middleware' => 'auth'], function (){
 
-Route::get('/teste', 'CodeEduBookController@index');
+    Route::resource('categories', 'CategoriesController', ['except' => 'show']);
+
+    Route::resource('books', 'BooksController', ['except' => 'show']);
+
+    Route::group(['prefix' => 'trashed', 'as' => 'trashed.'], function (){
+        Route::resource('books', 'BooksTrashedController', ['except' => ['create','store','edit','destroy']
+        ]);
+    });
+});
